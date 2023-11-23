89654e93a5f224b8b371ae2aa00edac11e1248aa
chenxinyu <465488636@qq.com>
2023-11-23 13:46:44 +0800

    feat: update React configuration

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Home from './pages/Home';
 
-const container = document.getElementById('root');
-const root = container && createRoot(container);
-root?.render(<Home />);
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(<Home />);
